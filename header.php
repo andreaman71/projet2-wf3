6d69f16cc7650208e57a9f4ab7cf54efebfc742c
Arthur Rebré <rebre.arthur@gmail.com>
2018-11-05 10:09:52 +0100

    Premier commit

---
+++ header.php
@@ -0,0 +1,20 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Projet 2</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+            <a class="nav-link" href="#">Accueil <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="inscription.php">S'inscrire</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link disabled" href="login.php">Connexion</a>
+        </li>
+        </ul>
+    </div>
+    </nav>
---
+++ inscription.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="fr">
+  <head>
+    <title>Projet 2</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
+  </head>
+  <body>
+    <?php include('header.php'); ?>
+
+    <main class="w-50 m-auto pt-5">
+        <form>
+            <div class="form-group">
+                <input type="email" class="form-control" aria-describedby="emailHelp" placeholder="Votre mail">
+            </div>
+            <div class="form-group">
+                <input type="firstname" class="form-control" aria-describedby="emailHelp" placeholder="Votre prénom">
+            </div>
+            <div class="form-group">
+                <input type="lastname" class="form-control" aria-describedby="emailHelp" placeholder="Votre nom">
+            </div>
+            <div class="form-group">
+                <input type="password" class="form-control" placeholder="Votre mot de passe">
+            </div>
+            <div class="form-group">
+                <input type="password-confirm" class="form-control" placeholder="Votre mot de passe (confirmation)">
+            </div>
+            <button type="submit" class="btn btn-primary">S'inscrire</button>
+        </form>
+    </main>
+      
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js" integrity="sha384-o+RDsa0aLu++PJvFqy8fFScvbHFLtbvScb8AjopnFD+iEQ7wo/CG0xlczd+2O/em" crossorigin="anonymous"></script>
+  </body>
+</html>
